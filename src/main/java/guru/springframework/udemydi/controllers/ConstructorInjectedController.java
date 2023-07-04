324bdcb72e4c38d1f100641e07c96dfc554cd6d1
ffe <felix.ferber@dscsag.com>
2023-07-04 14:52:09 +0200

    Renamed due to Typo

--- src/main/java/guru/springframework/udemydi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/udemydi/controllers/ConstructorInjectedController.java
@@ -1,12 +1,16 @@
 package guru.springframework.udemydi.controllers;
 
 import guru.springframework.udemydi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
-public class ConstructerInjectedController {
+@Controller
+public class ConstructorInjectedController {
 
+    @Autowired
     public GreetingService greetingService;
 
-    public ConstructerInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
--- src/test/java/guru/springframework/udemydi/controllers/ConstructorInjectedControllerTest.java
+++ src/test/java/guru/springframework/udemydi/controllers/ConstructorInjectedControllerTest.java
@@ -4,14 +4,12 @@ import guru.springframework.udemydi.services.GreetingServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+class ConstructorInjectedControllerTest {
 
-class ConstructerInjectedControllerTest {
-
-    ConstructerInjectedController controller;
+    ConstructorInjectedController controller;
     @BeforeEach
     void setUp() {
-        controller = new ConstructerInjectedController(new GreetingServiceImpl());
+        controller = new ConstructorInjectedController(new GreetingServiceImpl());
     }
 
     @Test
