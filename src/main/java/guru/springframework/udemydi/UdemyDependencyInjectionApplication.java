dda64a83d6c0b2bc0e00ea111b4f7da8d2bb6175
ffe <felix.ferber@dscsag.com>
2023-07-04 15:39:13 +0200

    Added Profile to Services

--- src/main/java/guru/springframework/udemydi/UdemyDependencyInjectionApplication.java
+++ src/main/java/guru/springframework/udemydi/UdemyDependencyInjectionApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.udemydi;
 
-import guru.springframework.udemydi.controllers.ConstructorInjectedController;
-import guru.springframework.udemydi.controllers.MyController;
-import guru.springframework.udemydi.controllers.PropertyInjectedController;
-import guru.springframework.udemydi.controllers.SetterInjectedController;
+import guru.springframework.udemydi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,23 +11,28 @@ public class UdemyDependencyInjectionApplication {
 	public static void main(String[] args) {
 		ApplicationContext appCTX = SpringApplication.run(UdemyDependencyInjectionApplication.class, args);
 
-		MyController myController = (MyController) appCTX.getBean("myController");
 
-		System.out.println("------------- Primary ----------------");
+		System.out.println("------------- Profile");
+		I18nController i18nController = (I18nController) appCTX.getBean("i18nController");
+		System.out.println(i18nController.helloWorld());
+
+
+		System.out.println("------------- Primary");
+		MyController myController = (MyController) appCTX.getBean("myController");
 		System.out.println(myController.helloWorld());
 
 
-		System.out.println("------------- Property ----------------");
+		System.out.println("------------- Property");
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) appCTX.getBean("propertyInjectedController");
 		System.out.println(propertyInjectedController.getGreeting());
 
 
-		System.out.println("------------- Setter ----------------");
+		System.out.println("------------- Setter");
 		SetterInjectedController setterInjectedController = (SetterInjectedController) appCTX.getBean("setterInjectedController");
 		System.out.println(setterInjectedController.getGreeting());
 
 
-		System.out.println("------------- Constructor ----------------");
+		System.out.println("------------- Constructor");
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) appCTX.getBean("constructorInjectedController");
 		System.out.println(constructorInjectedController.getGreeting());
 
