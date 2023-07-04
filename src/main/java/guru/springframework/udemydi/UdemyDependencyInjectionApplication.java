c911927100502e8ac5e43618862ead1e63da9c29
ffe <felix.ferber@dscsag.com>
2023-07-04 14:53:02 +0200

    Registered Controller and Service to Spring Frameworks

--- src/main/java/guru/springframework/udemydi/UdemyDependencyInjectionApplication.java
+++ src/main/java/guru/springframework/udemydi/UdemyDependencyInjectionApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.udemydi;
 
+import guru.springframework.udemydi.controllers.ConstructorInjectedController;
 import guru.springframework.udemydi.controllers.MyController;
+import guru.springframework.udemydi.controllers.PropertyInjectedController;
+import guru.springframework.udemydi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +19,24 @@ public class UdemyDependencyInjectionApplication {
 		String retVal = myController.helloWorld();
 
 		System.out.println(retVal);
+
+
+
+		System.out.println("------------- Property ----------------");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) appCTX.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+
+		System.out.println("------------- Setter ----------------");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) appCTX.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+
+		System.out.println("------------- Constructor ----------------");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) appCTX.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
+
+
 	}
 
 }
--- src/main/java/guru/springframework/udemydi/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/udemydi/controllers/PropertyInjectedController.java
@@ -1,9 +1,13 @@
 package guru.springframework.udemydi.controllers;
 
 import guru.springframework.udemydi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
 
+    @Autowired
     public GreetingService greetingService;
 
     public String getGreeting() {
